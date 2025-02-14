48723fec5974272baf0ed3382e68f1f00f9b57e7
Felix <afunmibi@gmail.com>
2025-02-14 13:25:00 +0100

    Started Portal development for Nonsuch Medicare

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="assets\bootstrap-5.3.3\dist\css\bootstrap.min.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" >
+	<title >Nonsuch Portal 2025</title>
+</head>
+<body>
+<div class=" py-3 container-fluid bg-gray">
+<h1 class="mx-5">Nonsuch Portal</h1>
+</div>
+
+<div class="container">
+	<div class="card">
+	<div class="card-header bg-gray text-center">
+	<h3 class="">Enrolment Portal </h3>
+</div>
+<div class="card-body">
+<form  action="" method="post">
+	<div class="grid ">
+  <div class="mb-2 g-col-6 g-col-md-4">
+    <span class="px-2">1. </span><label for="sname" class="form-label">Surname: </label>
+    <input type="text" class="form-control" id="sname" name="sname" placeholder="Enter your Surname">
+  </div>
+
+   <div class="mb-2 g-col-6 g-col-md-4">
+    <span class="px-2">2. </span><label for="fname:" class="form-label">First Name:</label>
+    <input type="text" class="form-control" id="fname" name="fname" placeholder="Enter your First Name">
+  </div>
+  <div class="mb-2 g-col-6 g-col-md-4">
+    <span class="px-2">3. </span><label for="lname:" class="form-label">Last Name:</label>
+    <input type="text" class="form-control" id="lname" name="lname" placeholder="Enter your Last Name">
+  </div>
+</div>
+
+<!-- other details -->
+	<div class="grid">
+  <div class="mb-2 g-col-6 g-col-md-4">
+    <span class="px-2">4. </span><label for="hospital" class="form-label">Choosen Hospital: </label>
+    <input type="text" class="form-control" id="hospital" name="hospital" placeholder="Enter your Hospital">
+  </div>
+
+   <div class="mb-2 g-col-6 g-col-md-4">
+    <span class="px-2">5. </span><label for="Email:" class="form-label">Email:</label>
+    <input type="email" class="form-control" id="email" name="email" placeholder="Enter your Email">
+  </div>
+  <div class="mb-2 g-col-6 g-col-md-4">
+    <span class="px-2">6. </span> <label for="Phone:" class="form-label">Phone:</label>
+    <input type="phone" class="form-control" id="phone" name="phone" placeholder="Enter your Phone Number">
+  </div>
+  <div class="mb-2 g-col-6 g-col-md-4">
+    <span class="px-2">7. </span> 
+    <input type="file" name="passport" id="passport">
+
+  </div>
+</div>
+
+
+
+
+  <button type="submit" class="btn btn-primary text-center align-center">Submit</button>
+</form>
+</div>
+</div>
+</div>
+
+<div class="card-footer text-center">
+	<p>nonsuchmedicare&copy2025</p>
+</div>
+</div>
+
+</div>
+</body>
+</html>
